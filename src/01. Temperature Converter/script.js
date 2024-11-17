f3450104731c3f64dc9c252b29c5b82637740a4c
Air <Acesparkle243@gmail.com>
2024-11-17 23:32:29 +0800

    fix formula

--- src/01. Temperature Converter/script.js
+++ src/01. Temperature Converter/script.js
@@ -7,14 +7,20 @@ document.addEventListener("DOMContentLoaded", () => {
   celcius.addEventListener("input", (event) => {
     const celciusValue = event.target.value;
     const toFarenheiht = parseFloat((celciusValue * 9) / 5 + 32);
+    const toFarenheihtFormatted = toFarenheiht.toFixed(3).replace(/\.?0+$/, "");
 
-    farenheiht.value = toFarenheiht.toFixed(3).replace(/\.?0+$/, "");
+    farenheiht.value = toFarenheihtFormatted;
+    farenheihtFormulaValue.textContent = toFarenheihtFormatted;
+    celciusFormulaValue.textContent = celciusValue;
   });
 
   farenheiht.addEventListener("input", (event) => {
     const farenheihtValue = event.target.value;
     const toCelcius = parseFloat(((farenheihtValue - 32) * 5) / 9);
+    const toCelciusFormatted = toCelcius.toFixed(3).replace(/\.?0+$/, "");
 
-    celcius.value = toCelcius.toFixed(3).replace(/\.?0+$/, "");
+    celcius.value = toCelciusFormatted;
+    celciusFormulaValue.textContent = toCelciusFormatted;
+    farenheihtFormulaValue.textContent = farenheihtValue;
   });
 });
